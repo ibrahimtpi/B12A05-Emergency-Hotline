d0674b647feeebf9483d0bc4a284e354d52b881d
Ibrahim Rana <rana.infonet@gmail.com>
2025-08-29 19:19:53 +0600

    Call Buttons Functionalities

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,3 +9,51 @@ for (const heartBtn of heartBtns) {
         availableHeart.innerText = totalHeartValue;
     });
 }
+
+
+// Call Button Functionality
+const callBtns = document.getElementsByClassName('call');
+for (const callBtn of callBtns) {
+    callBtn.addEventListener('click', function (ele) {
+        ele.preventDefault();
+        const serviceName = ele.target.parentNode.parentNode.children[1].innerText;
+        const serviceNumber = ele.target.parentNode.parentNode.children[3].innerText;
+
+        const coins = document.getElementById('coin-count');
+        const coinsValue = parseInt(coins.innerText);
+        if (coinsValue >= 20) {
+            let availableCoins = coinsValue - 20;
+            coins.innerText = availableCoins;
+
+            alert('Calling ' + serviceName + ' ' + serviceNumber + '...');
+
+            // add to call history
+            const callHistory = document.getElementById('call-history');
+            const listItem = document.createElement('li');
+            listItem.classList.add('mb-2');
+            listItem.innerHTML = `<div class="flex justify-between items-center text-md bg-[#fafafa] p-4">
+                                    <div>
+                                        <p class="font-bold">${serviceName}</p>
+                                        <p class="text-gray-600">${serviceNumber}</p>
+                                    </div>
+                                    <span class="text-gray-500">${currentTime()}</span>
+                                </div>`;
+            callHistory.appendChild(listItem);
+
+        } else {
+            alert('You do not have sufficient coins. To make a call you need at least 20 coins.');
+        }        
+    });
+}
+// get the current time hour: minute: second: AM/PM
+function currentTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const strTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
+    return strTime;
+}
